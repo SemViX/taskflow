75ced0ab12e9b6df622004e7cc754fec6e1d239d
SemViX <vitalsem.1973@gmail.com>
2026-08-31 14:37:24 +0300

    feat: register Project and Task models in admin with custom configurations

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Project
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("title", "owner", "color", "order", "created_at")
+    list_filter = ("owner",)
+    search_fields = ("title", "owner__username", "owner__email")
+    ordering = ("order", "created_at")
--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("title", "project", "priority", "deadline", "is_done", "order")
+    list_filter = ("is_done", "priority", "project")
+    search_fields = ("title", "project__title")
+    ordering = ("-priority", "order", "created_at")
+    list_select_related = ("project",)
