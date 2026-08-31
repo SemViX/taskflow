0413677145a95253e35ed5582e5e83ef45b548a3
SemViX <vitalsem.1973@gmail.com>
2026-08-31 12:36:20 +0300

    test: add tests for project list view functionality

--- projects/tests.py
+++ projects/tests.py
@@ -1,3 +1,21 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+User = get_user_model()
+
+
+class ProjectListViewTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="tester", password="testpass123")
+        self.client.force_login(self.user)
+
+    def test_add_project_button_stays_visible_when_form_is_opened(self):
+        response = self.client.get(reverse("projects:list"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "+ Add Project List")
+        self.assertContains(response, 'id="project-form-container"')
+        self.assertContains(response, 'hx-target="#project-form-container"')
+        self.assertNotContains(response, 'hx-target="this"')
